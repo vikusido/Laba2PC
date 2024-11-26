c84d8a16188c586339ccbbcb88c5c6181e748198
vikusido <145844166+vikusido@users.noreply.github.com>
2024-11-26 14:54:23 +0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,64 +1,49 @@
 #include <iostream>   //для ввода/вывода
+#include <cstring>    //для работы со строками
 #include <fcntl.h>    //для работы с файловыми дескрипторами
-#include <unistd.h>   //для системных вызовов (read, write, close)
 #include <sys/stat.h> //для создания FIFO
+#include <unistd.h>   //для системных вызовов (read, write, close)
+#include <signal.h>   //для обработки сигнала
 #include <errno.h>    //для обработки ошибок
+#include <fstream>
 
 using namespace std;
 
 // преобразование пути в PATH
 #define PATH "/tmp/pingpong_fifo"
 
+// обработка сигнала SIGINT (ctrl+C)
+void handle_sigint(int sig)
+{
+    cout << "\nThe server is shutting down. Removing FIFO..." << endl;
+    unlink(PATH);
+    exit(0);
+}
+
 int main()
 {
-    cout << "Client is running... Type 'exit' to quit." << endl;
-    // флаг для отслеживания существования FIFO
-    bool FIFO_is_found = true;
+    // установка обработчика сигнала SIGINT
+    signal(SIGINT, handle_sigint);
 
+    // создаем FIFO, если его не существует
+    if (mkfifo(PATH, 0666) == -1 && errno != EEXIST)
+    {
+        perror("Error creating FIFO");
+        return 1;
+    }
+
+    cout << "Server is running... Waiting for the client's messages." << endl;
+     //создание файла с историей переписки
+    ofstream history("/tmp/chat_history.txt", ios::trunc);
+    if (!history) {
+        cerr << "Error opening history file!" << endl;
+        return 1;
+    }
+    history<<"Server is running... Waiting for the client's messages."<<endl;
     while (true)
     {
-        // проверка на существование FIFO
-        if (access(PATH, F_OK) == -1)
-        {
-            if (FIFO_is_found)
-            {
-                cout << "FIFO does not exist. Is the server running?" << endl;
-                FIFO_is_found = false;
-            }
-            sleep(1); // пауза между проверками
-            continue;
-        }
-        else
-        {
-            FIFO_is_found = true;
-        }
-
-        // запись в message ввода пользователя
-        string message;
-        cout << "Enter message (ping): ";
-        getline(cin, message);
-
-        // открываем FIFO для записи и записываем файловый дескриптор в fd
-        int fd = open(PATH, O_WRONLY);
-        // обработка ошибки открытия FIFO
-        if (fd == -1)
-        {
-            perror("Error opening FIFO for writing");
-            return 1;
-        }
-        // записвыаем ввод пользователя
-        write(fd, message.c_str(), message.length());
-        close(fd);
-
-        // проверяем не завершает ли работу пользователь
-        if (message == "exit")
-        {
-            cout << "Exit." << endl;
-            break;
-        }
-
-        // открываем FIFO для чтения
-        fd = open(PATH, O_RDONLY);
+        // открываем FIFO для чтения и записываем файловый дескриптор в fd
+        int fd = open(PATH, O_RDONLY);
         // обработка ошибки открытия FIFO
         if (fd == -1)
         {
@@ -69,24 +54,74 @@ int main()
         // массив для хранения считанных данных
         char buffer[1024] = {0};
         ssize_t bytes_read = read(fd, buffer, sizeof(buffer) - 1);
-        close(fd);
-
+        
         if (bytes_read > 0)
         {
             // добавляем символ конца строки и выводим сообщение
             buffer[bytes_read] = '\0';
-            cout << "Server response: " << buffer << endl;
+            cout << "Received: " << buffer << endl;
+            history << "Received: "<< buffer << endl;
+
+            // проверка не завершил ли работу пользователь
+            if (strcmp(buffer, "exit") == 0)
+            {
+                cout << "Client requested to exit. Shutting down server." << endl;
+                history << "Client requested to exit. Shutting down server." << endl;
+                close(fd);
+                break;
+            }
+            else if (strcmp(buffer, "history") == 0){
+                string outputer;
+                ifstream open_history("/tmp/chat_history.txt");
+                fd = open(PATH, O_WRONLY);
+                if (fd == -1)
+                {
+                    perror("Error opening FIFO for reading");
+                    return 1;
+                }
+                string history_to_client;
+                history_to_client+="--- Start history ---\n";
+                cout << "--- Start history ---" << endl;
+                while (getline(open_history, outputer)){
+                    history_to_client+=outputer + "\n";
+                    cout << outputer <<endl;
+                }
+                history_to_client+="--- End history ---\n";
+                write (fd, history_to_client.c_str(), history_to_client.length());
+                close(fd);
+                open_history.close();
+                cout << "--- End history ---" << endl;
+            }
+
+            // открываем FIFO для записи
+            fd = open(PATH, O_WRONLY);
+            if (fd == -1)
+            {
+                perror("Error opening FIFO for writing");
+                close(fd);
+                return 1;
+            }
+            // формируем строку с ответом и записываем её
+            if (strcmp(buffer, "history") !=0){
+                string response = "Wow, " + string(buffer) + "!";
+                history << response << endl;
+                write(fd, response.c_str(), response.length());
+            }
+            close(fd);
         }
         // обработка случаев, когда bytes_read не положителен
         else if (bytes_read == -1)
         {
             perror("Error reading from FIFO");
-        }
+            close(fd);
+ }
         else if (bytes_read == 0)
         {
             cout << "EOF or empty FIFO" << endl;
+            close(fd);
         }
     }
-
+    unlink(PATH); // yдаляем FIFO при завершении
     return 0;
 }
+            
